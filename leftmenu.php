81be62504af59db82b165a140c56be3f8e8182b4
bdbrandt <benjamin.brandt@nau.edu>
2016-04-07 10:47:44 -0500

    Upload of files on iDigBio server

---
+++ leftmenu.php
@@ -0,0 +1,84 @@
+<div id="">
+	<ul class="sitemenu" style="padding-top:8px;">
+		<li>
+			<a class="start" href="<?php echo $clientRoot; ?>/about/index.php">About Us</a>
+			<ul>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/about/vplants.php">vPlants Project</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/about/chicago.php">Chicago Region</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/about/partnership.php">vPlants Partnership</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/about/contact.php">Contact Us</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/about/credits.php">Credits</a>
+				</li>
+			</ul>
+		</li>
+	</ul>
+	<ul class="sitemenu" style="margin-top:6px;">
+		<li>
+			<a class="plant" href="<?php echo $clientRoot; ?>/plants/index.php">Plants</a>
+			<ul>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/chicagoplants.php">Chicago Plants</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/guide/index.php">Guide</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/glossary/index.php">Glossary</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/biology.php">Biology</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/diversity.php">Diversity</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/habitats.php">Habitats</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/origin.php">Origin</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/distribution.php">Distribution</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/names.php">Names</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/collections.php">Collections</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/plants/concern.php">Special Concern</a>
+				</li>
+			</ul>
+		</li>
+	</ul>
+	<ul class="sitemenu" style="margin-top:6px;">
+		<li>
+			<a class="start" href="<?php echo $clientRoot; ?>/resources/index.php">Resources</a>
+			<ul>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/resources/regionherbaria.php">Region Herbaria</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/resources/docs.php">Documents</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/resources/biblio.php">References</a>
+				</li>
+				<li>
+					<a href="<?php echo $clientRoot; ?>/resources/links.php">Links</a>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div><!-- end of #sitemenu -->
+
